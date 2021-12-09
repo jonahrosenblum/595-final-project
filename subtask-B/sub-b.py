bf69db149a14fb81101fc718cf01deec62e77d68
jonahrosenblum <jonaherosenblum@gmail.com>
2021-12-09 17:06:58 -0500

    add capabilities for semeval dataset

--- subtask-B/sub-b.py
+++ subtask-B/sub-b.py
@@ -3,13 +3,27 @@ from transformers import InputExample, InputFeatures
 from transformers import logging
 import tensorflow as tf
 
-import json
+import xmltodict, json
 
 logging.set_verbosity_error()
 
-# from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
+semeval_map = {'no': False, 'yes': True, 'unsure': 2, ' unsure': 2}
 label_map = {False: 0, True: 1}
+
+def read_xml(filename):
+    semeval_list = []
+    with open(filename) as f:
+        doc = xmltodict.parse(f.read())
+        for q in doc['root']['Question']:
+            if q['@QTYPE'] == "YES_NO":
+                for comment in q['Comment']:
+                    if comment['@ANNOTATION'] == 'good' and comment['@YES_NO'] in ['yes', 'no']:
+                        semeval_list.append({"question": q['Full_Question'], 
+                                             "passage": comment['Body'], 
+                                             "answer": semeval_map[comment['@YES_NO']]
+                                            })
+
+    return semeval_list
 
 def read_jsonl(filename):
     jsonl = []
@@ -18,7 +32,7 @@ def read_jsonl(filename):
             jsonl.append(json.loads(line))
     return jsonl
 
-def convert_data_to_examples(train, test, a, b, label): 
+def convert_data_to_examples(train, test, a, b, label):
     train_input_examples = [InputExample(guid=None, text_a = x[a], text_b = x[b], label = label_map[x[label]]) for x in train]
 
     validation_input_examples = [InputExample(guid=None, text_a = x[a], text_b = x[b], label = label_map[x[label]]) for x in test]
@@ -51,14 +65,14 @@ def examples_to_tf_dataset(examples, tokenizer, max_length=128):
         )
 
     def gen():
-        for f in features:
+        for feature in features:
             yield (
                 {
-                    'input_ids': f.input_ids,
-                    'attention_mask': f.attention_mask,
-                    'token_type_ids': f.token_type_ids,
+                    'input_ids': feature.input_ids,
+                    'attention_mask': feature.attention_mask,
+                    'token_type_ids': feature.token_type_ids,
                 },
-                f.label,
+                feature.label,
             )
 
     return tf.data.Dataset.from_generator(
@@ -74,18 +88,7 @@ def examples_to_tf_dataset(examples, tokenizer, max_length=128):
         ),
     )
 
-
-
-if __name__ == '__main__':
-    model = TFBertForSequenceClassification.from_pretrained('bert-base-uncased')
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-    # tokenizer = AutoTokenizer.from_pretrained("ishan/bert-base-uncased-mnli")
-    # model = AutoModelForSequenceClassification.from_pretrained("ishan/bert-base-uncased-mnli")
-
-    train = read_jsonl('small-train.json')
-    test = read_jsonl('small-test.json')
-
+def train_model(model, tokenizer, train, test):
     train_input_examples, validation_input_examples = convert_data_to_examples(train, test, 'question', 'passage', 'answer')
 
     train_data = examples_to_tf_dataset(train_input_examples, tokenizer)
@@ -94,8 +97,22 @@ if __name__ == '__main__':
     validation_data = examples_to_tf_dataset(validation_input_examples, tokenizer)
     validation_data = validation_data.batch(32)
 
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=3e-5, epsilon=1e-08, clipnorm=1.0), 
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
-              metrics=[tf.keras.metrics.SparseCategoricalAccuracy('accuracy')])
-
     model.fit(train_data, epochs=3, validation_data=validation_data)
+
+
+if __name__ == '__main__':
+
+    model = TFBertForSequenceClassification.from_pretrained('bert-base-uncased')
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=3e-5, epsilon=1e-08, clipnorm=1.0), 
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+                  metrics=[tf.keras.metrics.SparseCategoricalAccuracy('accuracy')])
+
+    train_l = [read_jsonl('small-train.json'), read_xml('trial.xml')]
+    test_l = [read_jsonl('small-test.json'), read_xml('trial.xml')]
+    
+    for train, test in zip(train_l, test_l):
+        train_model(model, tokenizer, train, test)
+
+    
