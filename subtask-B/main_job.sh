a47b22ab409f9e3ebcab9c15160c36ef10d1fa3c
jonahrosenblum <jonaherosenblum@gmail.com>
2021-12-09 17:09:49 -0500

    add main job script

---
+++ subtask-B/main_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# JOB HEADERS HERE
+
+#SBATCH --partition=gpu
+#SBATCH --time=0-01:00:00
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=2
+#SBATCH --mem-per-gpu=16GB
+#SBATCH --account=eecs595s001f21_class
+
+# set up job
+module load cuda
+module load cudnn
+module load python/3.8.7
+pushd /home/jonaher/595-final-project/subtask-B
+
+source env/bin/activate
+
+# run subtask 2
+python subtask2.py
